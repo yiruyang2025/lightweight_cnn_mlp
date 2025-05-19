65844284fb53837ae1df274ae9384f6ac5f35923
YiruYang <162698398+yiruyang2025@users.noreply.github.com>
2025-05-19 04:43:17 +0200

    Update multimodal_tf.py

--- models/multimodal_tf.py
+++ models/multimodal_tf.py
@@ -1,19 +1,20 @@
-from tensorflow.keras import layers, Model
 from tensorflow.keras.applications import EfficientNetB0
+from tensorflow.keras import layers, models
 
-def build_multimodal_model(image_shape=(224, 224, 3), spectrum_dim=13, num_classes=10):
-    # Image input via EfficientNet
-    image_input = layers.Input(shape=image_shape)
-    cnn_base = EfficientNetB0(include_top=False, weights='imagenet', input_shape=image_shape, pooling='avg')
+def build_multimodal(input_shape_img=(224, 224, 3), input_shape_feat=(16,), num_classes=40):
+    # CNN branch
+    cnn_base = EfficientNetB0(include_top=False, weights='imagenet', pooling='avg', input_shape=input_shape_img)
     cnn_base.trainable = False
-    image_features = cnn_base(image_input)
+    img_input = layers.Input(shape=input_shape_img)
+    cnn_features = cnn_base(img_input)
 
-    # Spectrum input via MLP
-    spectrum_input = layers.Input(shape=(spectrum_dim,))
-    x_spectrum = layers.Dense(64, activation='relu')(spectrum_input)
+    # MLP branch
+    feat_input = layers.Input(shape=input_shape_feat)
+    mlp_features = layers.Dense(32, activation='relu')(feat_input)
 
     # Combine
-    combined = layers.concatenate([image_features, x_spectrum])
-    outputs = layers.Dense(num_classes, activation='softmax')(combined)
+    combined = layers.Concatenate()([cnn_features, mlp_features])
+    x = layers.Dense(64, activation='relu')(combined)
+    output = layers.Dense(num_classes, activation='softmax')(x)
 
-    return Model(inputs=[image_input, spectrum_input], outputs=outputs)
+    return models.Model(inputs=[img_input, feat_input], outputs=output)
