f5dc5e90e5f5ad300f750b4ce8a2ec7510ab4393
YiruYang <162698398+yiruyang2025@users.noreply.github.com>
2025-05-19 15:05:21 +0200

    Update cnn_tf.py

--- models/cnn_tf.py
+++ models/cnn_tf.py
@@ -1,10 +1,14 @@
-from tensorflow.keras.applications import EfficientNetB0
+import tensorflow as tf
 from tensorflow.keras import layers, models
 
-def build_cnn(input_shape=(224, 224, 3), num_classes=40):
-    base_model = EfficientNetB0(include_top=False, weights='imagenet', pooling='avg', input_shape=input_shape)
-    base_model.trainable = False
-    inputs = layers.Input(shape=input_shape)
-    x = base_model(inputs)
-    outputs = layers.Dense(num_classes, activation='softmax')(x)
-    return models.Model(inputs, outputs)
+def create_cnn_model(input_shape=(128, 128, 1), num_classes=10):
+    model = models.Sequential([
+        layers.Conv2D(32, (3, 3), activation='relu', input_shape=input_shape),
+        layers.MaxPooling2D((2, 2)),
+        layers.Conv2D(64, (3, 3), activation='relu'),
+        layers.MaxPooling2D((2, 2)),
+        layers.Flatten(),
+        layers.Dense(64, activation='relu'),
+        layers.Dense(num_classes, activation='softmax')
+    ])
+    return model
