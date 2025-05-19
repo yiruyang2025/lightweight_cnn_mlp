79aef39c032c5f126b79f80e09d779b6e3e96f4f
YiruYang <162698398+yiruyang2025@users.noreply.github.com>
2025-05-19 04:43:03 +0200

    Update cnn_tf.py

--- models/cnn_tf.py
+++ models/cnn_tf.py
@@ -1,12 +1,10 @@
 from tensorflow.keras.applications import EfficientNetB0
-from tensorflow.keras import layers, Model
+from tensorflow.keras import layers, models
 
-def build_efficientnet_cnn(input_shape=(224, 224, 3), num_classes=10):
-    base_model = EfficientNetB0(include_top=False, weights='imagenet', input_shape=input_shape, pooling='avg')
-    base_model.trainable = False  # Freeze the base
-
+def build_cnn(input_shape=(224, 224, 3), num_classes=40):
+    base_model = EfficientNetB0(include_top=False, weights='imagenet', pooling='avg', input_shape=input_shape)
+    base_model.trainable = False
     inputs = layers.Input(shape=input_shape)
     x = base_model(inputs)
     outputs = layers.Dense(num_classes, activation='softmax')(x)
-
-    return Model(inputs, outputs)
+    return models.Model(inputs, outputs)
